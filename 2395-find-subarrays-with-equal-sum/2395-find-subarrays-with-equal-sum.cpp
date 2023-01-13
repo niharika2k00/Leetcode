679b2ab65adff53ca871e8811a7cce2f465e310c
Niharika Dutta <64894768+niharika2k00@users.noreply.github.com>
2023-01-13 16:58:36 +0200

    Time: 0 ms (100%), Space: 7.8 MB (29.46%) - LeetHub

---
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool findSubarrays(vector<int>& nums) {
+        int len = nums.size(), sum =0 ;
+        unordered_set<int> set;
+
+        for(int i=0 ; i<len-1 ; i++){
+            sum = nums[i] + nums[i+1];
+
+            if(set.find(sum) == set.end()) // not found
+                set.insert(sum);
+            else {
+                cout << "Sum = " << sum;
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
