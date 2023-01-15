2edc397f2cc302392d9301ba6bd44f1b4df769d4
Niharika Dutta <64894768+niharika2k00@users.noreply.github.com>
2023-01-15 10:49:41 +0200

    using DFS for finding the minimum char.

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
-    int abc(int i,vector<int>& p){
-        if(i==p[i]) return i;
-        p[i] = abc(p[i],p);
-        return p[i];
+    char findMinUsingDFS(unordered_map<char, vector<char>> &adj, char currChar, vector<int> &visited){
+        visited[currChar - 'a'] = 1; // mark as visited
+        char minChar = currChar;
+
+        for(auto &ele: adj[currChar]){
+            if(visited[ele - 'a'] == 0)
+                minChar = min(minChar , findMinUsingDFS(adj, ele, visited));
+        }
+
+        return minChar;
     }
+
     string smallestEquivalentString(string s1, string s2, string baseStr) {
-        vector<int> p(26,0);
-        for(int i=0;i<26;i++) p[i]=i;
-        for(int i=0;i<s1.length();i++){
-            int x = abc(s1[i]-'a',p);
-            int y = abc(s2[i]-'a',p);
-            p[max(x,y)] = min(x,y);
+        unordered_map<char, vector<char>> adj;
+        string result;
+        int len = s1.size();
+
+        for(int i=0; i<len; i++){
+            char u = s1[i];
+            char v = s2[i];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
-        for(int i=0;i<baseStr.length();i++){
-            baseStr[i]=abc(baseStr[i]-'a',p)+'a';
+
+        for(int i=0 ; i<baseStr.size() ; i++){
+            char ch = baseStr[i];
+            vector<int> visited(26,0);
+
+            char val = findMinUsingDFS(adj, ch, visited);
+            result += val;
         }
-        return baseStr;
+
+        return result;
     }
 };
+
+
+
+
+
+
