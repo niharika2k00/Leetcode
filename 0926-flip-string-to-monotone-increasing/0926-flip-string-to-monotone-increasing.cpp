455b2bebb78acb69ac0a958a905989ee9389499a
Niharika Dutta <64894768+niharika2k00@users.noreply.github.com>
2023-01-17 23:08:22 +0200

    Time: 21 ms (92.39%), Space: 11 MB (82.49%) - LeetHub

---
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minFlipsMonoIncr(string s) {
+
+        int len = s.size(), tzero=0, tone=0, mini = 0;
+
+// Count 1's at LHS and 0's at RHS
+        for(int i=0; i<len ; i++){
+            if(s[i] == '0'){ 
+                tzero++ ; 
+                tzero = min(tzero , tone);
+            }
+            else {
+                tone++; 
+                
+            }
+            
+        }
+
+        return tzero;
+    }
+};
