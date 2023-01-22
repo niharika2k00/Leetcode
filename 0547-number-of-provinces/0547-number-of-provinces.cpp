566d1ee35e27ecab05bb41689af009bf8592146f
Niharika Dutta <64894768+niharika2k00@users.noreply.github.com>
2023-01-22 20:41:38 +0200

    Time: 29 ms (59.92%), Space: 16.4 MB (6.86%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,36 +1,72 @@
 class Solution {
 public:
-    void DFS(unordered_map<int, vector<int>> &adj, vector<bool>&visited, int u){
+    // void DFS(unordered_map<int, vector<int>> &adj, vector<bool>&visited, int u){
+
+    //     visited[u] = true;
+        
+    //     for(auto &v: adj[u]){
+    //         if(!visited[v]){
+    //             DFS(adj, visited, v);
+    //         }
+    //     }
+    // }
+
+    // int findCircleNum(vector<vector<int>>& isConnected) {
+        
+    //     unordered_map<int, vector<int>> adj;
+    //     int totalVertex = isConnected.size(), count=0;
+    //     vector<bool> visited(totalVertex,false);
+
+    // // Create the adjacency list
+    //     for(int i=0; i<totalVertex; i++){
+    //         for(int j=0; j<totalVertex; j++){
+    //             if(isConnected[i][j] == 1)
+    //                 adj[i].push_back(j);
+    //                 adj[j].push_back(i);
+    //         }
+    //     }
+
+    //     for(int i=0; i<totalVertex; i++){
+    //         if(!visited[i]){
+    //             count++;
+    //             DFS(adj, visited, i);
+    //         }
+    //     }
+
+    //     return count;
+    // }
+
+
+
+    void DFS(vector<vector<int>> &isConnected, vector<bool>&visited, int u){
 
         visited[u] = true;
         
-        for(auto &v: adj[u]){
-            if(!visited[v]){
-                DFS(adj, visited, v);
+        for(int v=0; v<isConnected.size(); v++){
+            if(!visited[v] && isConnected[u][v]){
+                DFS(isConnected, visited, v);
             }
         }
     }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
-        
-        unordered_map<int, vector<int>> adj;
+
         int totalVertex = isConnected.size(), count=0;
         vector<bool> visited(totalVertex,false);
 
     // Create the adjacency list
-        for(int i = 0; i<totalVertex; i++) {
-            for(int j = 0; j<totalVertex; j++) {
-                if(isConnected[i][j] == 1) {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
+        for(int i=0; i<totalVertex; i++){
+            for(int j=0; j<totalVertex; j++){
+                if(isConnected[i][j] == 1)
+                    isConnected[i].push_back(j);
+                    isConnected[j].push_back(i);
             }
         }
 
         for(int i=0; i<totalVertex; i++){
             if(!visited[i]){
                 count++;
-                DFS(adj, visited, i);
+                DFS(isConnected, visited, i);
             }
         }
 
