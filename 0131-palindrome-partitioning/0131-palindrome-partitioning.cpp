26768c10ad5e805fb65bc440d4dba1335765ba27
Niharika Dutta <64894768+niharika2k00@users.noreply.github.com>
2023-01-29 15:29:04 +0200

    Time: 140 ms (62.55%), Space: 57.5 MB (58.25%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,45 @@
+
+class Solution {
+public:
+    
+     bool isPalindrome(string s, int start, int end) {
+        while (start <= end) {
+          if (s[start++] != s[end--])
+            return false;
+        }
+        return true;
+    }
+    
+    void helper(int ind, string s, vector<string> &path, vector<vector<string>> &res) {
+        
+        if (ind == s.size()) {
+          res.push_back(path);
+          return;
+        }
+        
+        for(int i = ind; i < s.size(); i++) {
+
+            // check whether the substring before partion is palindrome
+            if(isPalindrome(s, ind, i)) {
+                path.push_back(s.substr(ind, i - ind + 1));
+                helper(i + 1, s, path, res);
+                path.pop_back();
+            }
+        }
+    }
+    
+    // Main function
+    vector<vector<string>> partition(string s) {
+        vector<string> path;  // stores the substring
+        vector<vector<string>> res;
+
+        helper(0, s, path, res);
+        return res;
+        
+    }
+};
+
+
+
+
+
