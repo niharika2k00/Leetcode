3c0dd7fb67a29a3fc67929b8a9a7f98ddea01dc2
Niharika Dutta <64894768+niharika2k00@users.noreply.github.com>
2023-02-02 16:37:27 +0200

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -6,19 +6,28 @@ using namespace std;
 class Solution {
 public:
 
-    bool bipartiteDFS(int curr, vector<int>adj[], vector<int>&color, int currColor){
+    bool bipartiteBFS(int curr, vector<int>adj[], vector<int>&color, int currColor){
         
-        color[curr] = currColor;
+        queue<int> que;
         
-        for(auto &it: adj[curr] ){
-            if(color[it] == currColor)  // same color adj node
-                return false;
-                
-            if(color[it] == -1) {  // never colored  
-                int colorOfV = !currColor;
-                
-                if(bipartiteDFS(it, adj, color, colorOfV) == false)
+        if(color[curr] == -1)
+            color[curr] = currColor;
+            
+        que.push(curr);
+        
+        while(!que.empty()){
+            int u = que.front();
+            que.pop();
+            
+            for(auto &it: adj[u]){
+                if(color[it] == color[u])
                     return false;
+                
+                if(color[it] == -1){
+                    color[it] = !color[u];
+                    
+                    que.push(it);
+                }
             }
         }
         
@@ -31,12 +40,12 @@ public:
 	    
 	    vector<int> color(V, -1);
 	    
-	    for(int i=0; i<V; i++){
-	        if(color[i] == -1)
-	            if(bipartiteDFS(i, adj, color, 1) == false)
-	                return false;
-	    }
-	    
+        for(int i=0; i<V; i++){
+            if(color[i] == -1)
+            if(bipartiteBFS(i, adj, color, 1) == false)
+	            return false;
+        }
+
 	    return true;
 	}
 
