64a5d129a9933585fa7cf3d983b7ad7e6bdc437c
Niharika Dutta <64894768+niharika2k00@users.noreply.github.com>
2023-08-07 19:00:31 +0530

    Time: 77 ms (97.02%), Space: 67.8 MB (8.51%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+// Kaldane's Algotithm
+    int helper1(vector<int> &vec, int len){
+        int currSum=0, maxSum=0, count=0;
+        // All Negetive elements present in the array  [-1]  OR  [-2,-1]
+        for (int i = 0; i < len; i++)
+            if (vec[i] < 0)
+                count++;
+
+        if (count == len)
+            return *max_element(vec.begin(), vec.end());
+
+        for(int i=0; i<len; i++){
+            currSum = currSum + vec[i];
+
+            if(currSum < 0)
+                currSum = 0;
+
+            maxSum = max(currSum, maxSum);
+        }
+        return maxSum;
+    }
+
+// Using 1D DP
+    int helper2(vector<int> &vec, int len){
+        int currSum=0, maxSum=0;
+        
+        for(int i=0; i<len; i++){
+
+        }
+        return maxSum;
+    }
+
+    int maxSubArray(vector<int>& nums) {
+        int len = nums.size();
+
+        return helper1(nums, len);
+        // return helper2(nums, len);
+    }
+};
