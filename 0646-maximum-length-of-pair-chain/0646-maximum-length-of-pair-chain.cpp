9cc15276cde11122cbd71c15688267c53675c654
Niharika Dutta <64894768+niharika2k00@users.noreply.github.com>
2023-08-31 22:17:32 +0530

    Time: 70 ms (51.8%), Space: 24.4 MB (28.86%) - LeetHub

---
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+// Similar to https://practice.geeksforgeeks.org/problems/n-meetings-in-one-room-1587115620/1 greedy problem
+
+    static bool comp(const vector<int>& vec1, const vector<int>& vec2){
+        return vec1[1] < vec2[1];
+    }
+
+    int findLongestChain(vector<vector<int>>& pairs) {
+        int res=1, lastPairEnd=0;
+        sort(pairs.begin(), pairs.end(), comp); // sort the pairs by last element [1]
+
+        for(auto it: pairs){
+            cout << it[0] << " " << it[1] << endl;
+        }
+
+        lastPairEnd = pairs[0][1];
+
+        for(int i=1; i<pairs.size(); i++){
+            if(pairs[i][0] > lastPairEnd){
+                res++;
+                lastPairEnd = pairs[i][1];
+            }
+        }
+
+        return res;
+    }
+};
