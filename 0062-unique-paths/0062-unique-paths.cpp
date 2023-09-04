821865584f32503de216c8b050e562803a08bd5e
Niharika Dutta <64894768+niharika2k00@users.noreply.github.com>
2023-09-04 21:53:08 +0530

    Time: 0 ms (100%), Space: 6.5 MB (25.17%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
-    int path(int m, int n, vector<vector<int>>&dp){
-        if(m==0 || n==0) 
+    int path(int i, int j, int m, int n, vector<vector<int>>&dp){
+        // as zero index
+        if(i == m-1 && j == n-1) { 
+            cout << i << " " << j << endl;
             return 1;
+        }
 
-        if(dp[m][n]!=-1)
-            return dp[m][n];
+        if(i >= m || j >= n) // boundary exceeds
+            return 0;
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
             
-        return dp[m][n]=path(m-1,n,dp) + path(m,n-1,dp); // right + down
+        return dp[i][j] = path(i+1, j, m, n, dp) + path(i, j+1, m, n, dp); // right + down
     }
 
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return path(m-1,n-1,dp); 
+        return path(0, 0, m, n, dp); 
     }
 };
