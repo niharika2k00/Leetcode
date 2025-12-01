c9b3963d08bded0db92122ac69a5203e8a1bd9c7
Niharika Dutta <niharika@turbot.com>
2025-12-01 19:59:09 +0530

    Time: 31 ms (5.71%), Space: 47.2 MB (5.16%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,20 +1,36 @@
 class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
-      HashMap<Character, Integer> hm = new HashMap<>(26);
+  public boolean canConstruct(String ransomNote, String magazine) {
+    // Method 1
+    HashMap<Character, Integer> hm = new HashMap<>(26);
 
-      for (char i: magazine.toCharArray()) {
-        hm.put(i, hm.getOrDefault(i, 0) + 1);
-      }
-
-      for (char i: ransomNote.toCharArray()) {
-        System.out.println("char: "+ i + " value: "+ hm.get(i));
-        // if key available and frequency(value) more than 0, decrement by 1
-        if (hm.containsKey(i) && hm.get(i) > 0)
-          hm.put(i, hm.get(i) - 1);
-        else
-          return false;
-      }
-
-      return true;
+    for (char i : magazine.toCharArray()) {
+      hm.put(i, hm.getOrDefault(i, 0) + 1);
     }
+
+    for (char i : ransomNote.toCharArray()) {
+      System.out.println("char: " + i + " value: " + hm.get(i));
+      // if key available and frequency(value) more than 0, decrement by 1
+      if (hm.containsKey(i) && hm.get(i) > 0)
+        hm.put(i, hm.get(i) - 1);
+      else
+        return false;
+    }
+
+    return true;
+
+    // Method 2
+    // int[] frequencyArr = new int[26];
+
+    // for (char c : magazine.toCharArray())
+    //   frequencyArr[c - 'a']++;
+
+    // for (char c : ransomNote.toCharArray()) {
+    //   if (!(frequencyArr[c - 'a'] > 0))
+    //     return false;
+
+    //   frequencyArr[c - 'a']--;  
+    // }
+
+    // return true;
+  }
 }
