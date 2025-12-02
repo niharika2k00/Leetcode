03ac462e1ead9b7cf9fab6e00d106435005d60b2
Niharika Dutta <niharika@turbot.com>
2025-12-02 21:45:44 +0530

    Time: 1 ms (99.81%), Space: 43.6 MB (24.98%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -2,24 +2,32 @@ class Solution {
   public String addBinary(String a, String b) {
     int i = a.length() - 1;
     int j = b.length() - 1;
-    StringBuilder res = new StringBuilder();
+    StringBuilder ans = new StringBuilder();
     int carry = 0;
 
-    while (i>=0 || j>=0 || carry==1) {
+    // loop untill reaches 0 index and no carry-over value left
+    while (i >= 0 || j >= 0 || carry == 1) {
       int sum = carry;
 
-      if(i>=0) sum += a.charAt(i--) - '0';
-      if(j>=0) sum += b.charAt(j--) - '0';
+      if (i >= 0) {
+        sum += a.charAt(i) - '0'; // converts char to int, 1 - 0 equals to 49 - 48 || 48 - 48
+        i--;
+      }
 
-      res.append(sum % 2);
+      if (j >= 0) {
+        sum += b.charAt(j) - '0';
+        j--;
+      }
+
+      ans.append(sum % 2); // mod with 2 as its binary
       carry = sum / 2;
     }
 
-    return res.reverse().toString();
+    return ans.reverse().toString();
   }
-} 
+}
 
-// 1 + 0 = 0
-// 0 + 1 = 0
 // 0 + 0 = 0
+// 1 + 0 = 1
+// 0 + 1 = 1
 // 1 + 1 = 10
