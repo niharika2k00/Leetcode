798ece798655dee0ddc2585b352f42f734a86764
Niharika Dutta <niharika@turbot.com>
2025-12-02 23:01:56 +0530

    Time: 14 ms (72.72%), Space: 93.6 MB (18.36%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,20 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-      Arrays.sort(nums);
+  public boolean containsDuplicate(int[] nums) {
+    // Time Complexity: O(nlogn)
+    // Arrays.sort(nums);
+    // for (int i = 1; i < nums.length; i++) {
+    //   if (nums[i - 1] == nums[i])
+    //     return true;
+    // }
 
-      for (int i=1; i<nums.length; i++) {
-        if (nums[i-1] == nums[i]) return true;
-      }
-      
-      return false;
+    // Time Complexity: O(n)
+    HashSet<Integer> hm = new HashSet<>();
+    for (int num : nums) {
+      if (hm.contains(num))
+        return true;
+      hm.add(num);
     }
+
+    return false;
+  }
 }
