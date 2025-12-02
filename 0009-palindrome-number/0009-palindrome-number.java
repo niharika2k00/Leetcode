e780d3a4d3aa02d072af031b38aa4ff0a26abf54
Niharika Dutta <niharika@turbot.com>
2025-12-03 02:10:38 +0530

    Time: 5 ms (70.8%), Space: 45.9 MB (32.36%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+      int reverse = 0, temp = x;
+
+      if (x < 0) return false;
+
+      while (temp != 0) {
+        reverse = (reverse * 10) + (temp % 10);
+        temp = temp / 10;
+      }
+
+      if (reverse == x) return true;
+      return false;
+    }
+}
