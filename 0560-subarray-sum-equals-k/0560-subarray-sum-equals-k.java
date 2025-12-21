85b113b2e72b025fce933fafb94d4f4acebfc867
Niharika Dutta <niharika@turbot.com>
2025-12-21 22:03:36 +0530

    Optimize subarraySum method implementation
    Refactor subarraySum method for clarity and efficiency.

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,42 +1,32 @@
-import java.util.HashMap;
-
-/*
- * @lc app=leetcode id=560 lang=java
- *
- * [560] Subarray Sum Equals K
- */
-
-// @lc code=start
 class Solution {
-    public int subarraySum(int[] nums, int k) {
-        int sum = 0, len = nums.length, i, j, count = 0, currSum = 0;
-
-        //  BRUTE - FORCE , Time Complexity : O(n^2)
-        /* for (i = 0; i < len; i++) {
-            sum = 0;
-            for (j = i; j < len; j++) {
-                sum = sum + nums[j];
-                if (sum == k)
-                    count++;
-            }
-        } */
-
-        //  OPTIMIZED APPROACH , Time Complexity : O(n)
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-
-        for (i = 0; i < len; i++) {
-            currSum = currSum + nums[i];
-
-            if (currSum == k)
+  public int subarraySum(int[] nums, int k) {
+    //  BRUTE - FORCE , Time Complexity : O(n^2)
+    /* for (i = 0; i < len; i++) {
+        sum = 0;
+        for (j = i; j < len; j++) {
+            sum = sum + nums[j];
+            if (sum == k)
                 count++;
-
-            if (hashMap.containsKey(currSum - k))
-                count = count + hashMap.get(currSum - k); //  hashMap[curr-k]  means the value part of the element.
-
-            hashMap.put(currSum, hashMap.getOrDefault(currSum, 0) + 1);
         }
+    } */
 
-        return count;
+    // OPTIMIZED APPROACH , Time Complexity : O(n)
+    HashMap<Integer, Integer> map = new HashMap<>(); // sum -> frequency
+    int count = 0, prefixSum = 0;
+
+    // initialize the map with {0 → 1} to handle the case when the subarray starts from index 0
+    map.put(0, 1);
+
+    for (int j = 0; j < nums.length; j++) {
+      prefixSum += nums[j];
+
+      if (map.containsKey(prefixSum - k)) {
+        count += map.get(prefixSum - k);
+      }
+
+      map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
     }
+
+    return count;
+  }
 }
-// @lc code=end
