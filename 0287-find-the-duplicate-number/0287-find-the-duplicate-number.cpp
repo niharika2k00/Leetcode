79d188c9690e4cc12b9bc210900d47ad789cd836
Niharika Dutta <niharika@turbot.com>
2026-01-20 02:33:23 +0530

    Time: 0 ms (100%), Space: 64.9 MB (99.17%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-
-// Method: Making visited values negative
+    // Method: Making visited values negative
     int findDuplicate(vector<int>& nums) {
         int len = nums.size();
 
-        for(int i=0; i<len; i++){
+        for (int i = 0; i < len; i++) {
             int idx = abs(nums[i]); // value
 
-            if(nums[idx] < 0)
+            if (nums[idx] < 0)
                 return idx;
             nums[idx] = -nums[idx];
         }
@@ -16,45 +15,45 @@ public:
         return len;
     }
 
-// METHOD - I
-// Time Complexity : O(n logn)                   Don't do it :)
-// Space Complexity : O(1)
-        //  int len = nums.size(), i;
-        //  sort(nums.begin(), nums.end()); //  Java :   Arrays.sort(nums);
+    // METHOD - I
+    // Time Complexity : O(n logn)                   Don't do it :)
+    // Space Complexity : O(1)
+    //  int len = nums.size(), i;
+    //  sort(nums.begin(), nums.end()); //  Java :   Arrays.sort(nums);
 
-        //  for (i = 1; i < len; i++)
-        //      if (nums[i - 1] == nums[i])
-        //          return nums[i];
+    //  for (i = 1; i < len; i++)
+    //      if (nums[i - 1] == nums[i])
+    //          return nums[i];
 
-        //  return -1;
+    //  return -1;
 
-// METHOD - II
-// Time Complexity : O(n) [Hare & Tortoise ---> Cycle Detection ]
-// Space Complexity : O(1)
-        // while (nums[0] != nums[nums[0]])
-        //     swap(nums[0], nums[nums[0]]);
+    // METHOD - II
+    // Time Complexity : O(n) [Hare & Tortoise ---> Cycle Detection ]
+    // Space Complexity : O(1)
+    // while (nums[0] != nums[nums[0]])
+    //     swap(nums[0], nums[nums[0]]);
 
-        // return nums[0];
+    // return nums[0];
 
-// METHOD - III
-// Time Complexity : O(n) [Hare & Tortoise ---> Cycle Detection ]
-// Space Complexity : O(1)
-        // int slow = 0, fast = 0;
-        //  slow = nums[0];
-        //  fast = nums[0];
+    // METHOD - III
+    // Time Complexity : O(n) [Hare & Tortoise ---> Cycle Detection ]
+    // Space Complexity : O(1)
+    // int slow = 0, fast = 0;
+    //  slow = nums[0];
+    //  fast = nums[0];
 
-        //  do
-        //  {
-        //      slow = nums[slow];
-        //      fast = nums[nums[fast]]; // 2 Step Jump
-        //  } while (slow != fast);
+    //  do
+    //  {
+    //      slow = nums[slow];
+    //      fast = nums[nums[fast]]; // 2 Step Jump
+    //  } while (slow != fast);
 
-        //  fast = nums[0]; // Reassign with the first index
-        //  while (slow != fast)
-        //  {
-        //      slow = nums[slow];
-        //      fast = nums[fast];
-        //  }
+    //  fast = nums[0]; // Reassign with the first index
+    //  while (slow != fast)
+    //  {
+    //      slow = nums[slow];
+    //      fast = nums[fast];
+    //  }
 
-        //  return slow;
+    //  return slow;
 };
