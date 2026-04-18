b44a1f996b59e85fa6ef36609755b7ca06ae0ac1
Niharika Dutta <dniharika16@gmail.com>
2026-04-18 22:03:43 +0530

    Time: 472 ms (22.65%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  set n = N - 1;
+  RETURN (
+    # Write your MySQL query statement below.
+    select distinct salary from Employee
+    order by salary DESC
+    limit 1 offset n
+  );
+END
